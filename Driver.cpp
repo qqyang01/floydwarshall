dee36d5dfe0b8a8d216f9d24f1c65bf24595e916
akuphal <71189982+akuphal@users.noreply.github.com>
2020-09-25 15:17:41 -0500

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -1,13 +1,18 @@
-#include <iostream>
-#include <string>
+#include "Floyd.h"
 #include <limits>
-
+#pragma once
+int P[5][5];
+int D[5][5];
 using namespace std;
-
 int main() {
-  //TEST CASE 1
 	int inf = std::numeric_limits<int>::max();
 	int W[5][5] = { {0,1,inf,1,5},{9,0,3,2,inf},{inf,inf, 0,4,inf},{inf,inf,2,0,3},{3,inf,inf,inf,0} };
+	for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 5; j++)
+			D[i][j] = W[i][j];
+	floyd(5, W, D, P);
+	path(0, 0, P);
+}
   
 	/*for (int i = 0; i < size(W); i++) {
 		cout << endl;
