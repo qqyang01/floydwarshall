09a22561ad5dca36cba3c685813d2e5cd23d6e1f
akuphal <71189982+akuphal@users.noreply.github.com>
2020-09-25 16:42:17 -0500

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -1,20 +1,18 @@
 #include "Floyd.h"
 #include <limits>
+#include <iostream>
 #pragma once
 int P[5][5];
 int D[5][5];
 using namespace std;
 int main() {
-	int inf = std::numeric_limits<int>::max();
-	int W[5][5] = { {0,1,inf,1,5},{9,0,3,2,inf},{inf,inf, 0,4,inf},{inf,inf,2,0,3},{3,inf,inf,inf,0} };
+	int inf = 1000000;
+	//std::numeric_limits<int>::max();
+	int W[5][5] = { {0,1, inf,1,5},{9,0,3,2,inf},{inf, inf, 0,4,inf},{inf,inf,2,0,3},{3,inf,inf,inf, 0} };
 	for (int i = 0; i < 5; i++)
 		for (int j = 0; j < 5; j++)
 			D[i][j] = W[i][j];
-	floyd(5, W, D, P);
-	path(0, 0, P);
-}
-  
-	/*for (int i = 0; i < size(W); i++) {
+	for (int i = 0; i < size(W); i++) {
 		cout << endl;
 		for (int j = 0; j < size(W[0]); j++) {
 			if (W[i][j] == inf) {
@@ -23,12 +21,9 @@ int main() {
 			else {
 				cout << W[i][j] << " ";
 			}
-
 		}
-	}*/
-
-
-
-
-
-};
+	}
+	floyd(5, W, D, P);
+	printSolution(D);
+	path(0, 0, P);
+}
