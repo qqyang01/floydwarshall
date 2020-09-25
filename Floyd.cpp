0521e7c16f41ff66bb46a73da19fc8b3865c434d
akuphal <71189982+akuphal@users.noreply.github.com>
2020-09-25 16:40:31 -0500

    Update Floyd.cpp

--- Floyd.cpp
+++ Floyd.cpp
@@ -15,7 +15,7 @@ void floyd(int n, int W[5][5], int D[5][5], int P[5][5])
 	for(k = 0; k<n; k++)
 		for(j = 0; j<n; j++)
 			for (i = 0; i < n; i++) {
-				if (D[i][k] + D[k][j] < D[i][j]&& D[i][k] != 100 && D[k][j] !=100) {
+				if (D[i][k] + D[k][j] < D[i][j]&& D[i][k] != 100 && D[k][j] !=1000000) {
 					P[i][k] = P[k][j];
 					D[i][j] = D[i][k] + D[k][j];
 				}
@@ -34,14 +34,12 @@ void path(int q, int r, int P[5][5])
 
 void printSolution(int D[5][5])
 	{
-		cout << "The following matrix shows the shortest distances"
-			" between every pair of vertices \n";
 		for (int i = 0; i < 5; i++)
 		{
 			for (int j = 0; j < 5; j++)
 			{
-				if (D[i][j] == 100)
-					cout << "INF" << "     ";
+				if (D[i][j] == 1000000)
+					cout << "inf" << "     ";
 				else
 					cout << D[i][j] << "     ";
 			}
