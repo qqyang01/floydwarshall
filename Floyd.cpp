b3d833999e109416e0800011ef9da473abe4055a
qqyang01 <48069098+qqyang01@users.noreply.github.com>
2020-09-26 11:22:33 -0500

    done!

--- Floyd.cpp
+++ Floyd.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+#define N 3
 #pragma once
-void floyd(int n, int W[5][5], int D[5][5], int P[5][5])
+void floyd(int n, int W[N][N], int D[N][N], int P[N][N])
 {
 	int i, j, k;
 	for (i = 0; i < n; i++)
@@ -24,61 +25,61 @@ void floyd(int n, int W[5][5], int D[5][5], int P[5][5])
 }
 
 
-void printSolution(int D[5][5], int P[5][5])
+void printSolution(int D[N][N], int P[N][N])
 {
 	int setDist = 7;
 	int setDist2 = 2;
 	char fill = ' ';
 	cout << "Final Distances" << endl;
 	cout << "     ";
-	for (int i = 1; i < 6; i++) {
-		
+	for (int i = 1; i < N+1; i++) {
+
 		cout << setw(setDist) << setfill(fill) << i;
 	}
-	cout <<endl << "   -------------------------------" <<endl;
-	
-	for (int i = 0; i < 5; i++)
+	cout << endl << "   -------------------------------" << endl;
+
+	for (int i = 0; i < N; i++)
 	{
 		cout << "  ";
 		cout << i + 1 << "| ";
-		for (int j = 0; j < 5; j++)
+		for (int j = 0; j < N; j++)
 		{
 			if (D[i][j] == 1000000)
-				cout <<setw(setDist2)<< setfill(fill)<<"inf" << "     ";
+				cout << setw(setDist2) << setfill(fill) << "inf" << "     ";
 			else
-				cout <<setw(setDist2) << setfill(fill) << D[i][j] << "     ";
+				cout << setw(setDist2) << setfill(fill) << D[i][j] << "     ";
 		}
 		cout << endl;
 
 	}
 	cout << "Final Paths " << endl;
 	cout << "     ";
-	for (int i = 1; i < 6; i++) {
-		
+	for (int i = 1; i < N+1; i++) {
+
 		cout << setw(setDist) << setfill(fill) << i;
 	}
 	cout << endl << "   -------------------------------" << endl;
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < N; i++)
 	{
 		cout << "  ";
 		cout << i + 1 << "| ";
-		for (int j = 0; j < 5; j++)
+		for (int j = 0; j < N; j++)
 		{
 			if (P[i][j] == 1000000)
 				cout << setw(setDist2) << setfill(fill) << "inf" << "     ";
 			else
-				cout <<setw(setDist2) << setfill(fill)<< P[i][j] << "     ";
+				cout << setw(setDist2) << setfill(fill) << P[i][j] << "     ";
 		}
 		cout << endl;
 	}
 	cout << "Shortest Paths: Decoded " << endl;
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < 5; j++)
+		for (int j = 0; j < N; j++)
 		{
 			if (j != i && P[i][j] != -1)
 			{
-				cout <<  i + 1 << " to " << j + 1 << ": " << i + 1 << " ";
+				cout << i + 1 << " to " << j + 1 << ": " << i + 1 << " ";
 				path(P, i, j);
 				cout << j + 1 << endl;
 			}
@@ -86,11 +87,11 @@ void printSolution(int D[5][5], int P[5][5])
 	}
 }
 
-void path(int P[5][5], int i, int j)
+void path(int P[N][N], int i, int j)
 {
 	if (P[i][j] == 0)
 		return;
 
-	path(P, P[i][j]-1, j);
+	path(P, P[i][j] - 1, j);
 	cout << P[i][j] << " ";
 }
