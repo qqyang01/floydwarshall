3e6ad9933ece9403cb6eb7763b62fa982e872422
qqyang01 <48069098+qqyang01@users.noreply.github.com>
2020-09-26 09:52:19 -0500

    decodes shortest path successfully now...

--- Floyd.cpp
+++ Floyd.cpp
@@ -5,18 +5,18 @@ using namespace std;
 void floyd(int n, int W[5][5], int D[5][5], int P[5][5])
 {
 	int i, j, k;
-	for (i = 0; i <n; i++)
+	for (i = 0; i < n; i++)
 		for (j = 0; j < n; j++) {
 			if (i == j)
 				P[i][j] = 0;
 			D[i][j] = W[i][j];
 		}
 
-	for(k = 0; k<n; k++)
-		for(j = 0; j<n; j++)
+	for (k = 0; k < n; k++)
+		for (j = 0; j < n; j++)
 			for (i = 0; i < n; i++) {
-				if (D[i][k] + D[k][j] < D[i][j]&& D[i][k] != 1000000 && D[k][j] !=1000000) {
-					P[i][j] = k+1;
+				if (D[i][k] + D[k][j] < D[i][j] && D[i][k] != 1000000 && D[k][j] != 1000000) {
+					P[i][j] = k + 1;
 					D[i][j] = D[i][k] + D[k][j];
 				}
 			}
@@ -24,53 +24,53 @@ void floyd(int n, int W[5][5], int D[5][5], int P[5][5])
 
 
 void printSolution(int D[5][5], int P[5][5])
-	{
-	cout << "Final Distances" << endl;
-		for (int i = 0; i < 5; i++)
-		{
-			for (int j = 0; j < 5; j++)
-			{
-				if (D[i][j] == 1000000)
-					cout  <<"inf" << "     ";
-				else
-					cout << D[i][j] << "     ";
-			}
-			cout << endl;
-			
-		}
-		cout << "Final Paths " << endl;
-		for (int i = 0; i < 5; i++)
-		{
-			for (int j = 0; j < 5; j++)
-			{
-				if (P[i][j] == 1000000)
-					cout << "inf" << "     ";
-				else
-					cout << P[i][j] << "     ";
-			}
-			cout << endl;
-		}
-		cout << "Shortest Paths: Decoded " << endl;
-		for (int i = 0; i < 5; i++)
-		{
-			for (int j = 0; j < 5; j++)
-			{
-				if (j != i && P[i][j] != -1)
-				{
-					cout << "From vertex " << i + 1 << " to vertex " << j + 1 << ": " << i + 1 << " ";
-					path(P, i, j);
-					cout << j + 1 << endl;
-				}
-			}
-		}
-	}
-
-void path(int P[5][5], int i, int j)
 {
-	if (P[i][j] != 0) {
-		path(P, i, P[i][j]);
-		cout << "v " << P[i][j];
-		path(P, P[i][j], j);
+
+	cout << "Final Distances" << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (D[i][j] == 1000000)
+				cout << "inf" << "     ";
+			else
+				cout << D[i][j] << "     ";
+		}
+		cout << endl;
+
+	}
+	cout << "Final Paths " << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (P[i][j] == 1000000)
+				cout << "inf" << "     ";
+			else
+				cout << P[i][j] << "     ";
+		}
+		cout << endl;
+	}
+	cout << "Shortest Paths: Decoded " << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (j != i && P[i][j] != -1)
+			{
+				cout <<  i + 1 << " to " << j + 1 << ": " << i + 1 << " ";
+				path(P, i, j);
+				cout << j + 1 << endl;
+			}
+		}
 	}
 }
 
+void path(int P[5][5], int i, int j)
+{
+	if (P[i][j] == 0)
+		return;
+
+	path(P, P[i][j]-1, j);
+	cout << P[i][j] << " ";
+}
