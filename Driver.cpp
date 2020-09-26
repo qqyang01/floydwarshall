6076d5ba9707d312e43c660bd78228796eb63f0c
qqyang01 <48069098+qqyang01@users.noreply.github.com>
2020-09-26 11:22:06 -0500

    done!

--- Driver.cpp
+++ Driver.cpp
@@ -3,23 +3,28 @@
 #include <iostream>
 #include <iomanip>
 #pragma once
+#define N 3
 
-int P[5][5];
-int D[5][5];
+int P[N][N];
+int D[N][N];
 using namespace std;
 int main() {
 	int inf = 1000000;
-	int W[5][5] = { {0,1, inf,1,5},{9,0,3,2,inf},{inf, inf, 0,4,inf},{inf,inf,2,0,3},{3,inf,inf,inf, 0} };
+	int W[N][N] = { {0,4,5},{2,0,inf},{inf,-3, 0} };
 	//set D=W
-	for (int i = 0; i < 5; i++)
-		for (int j = 0; j < 5; j++)
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
 			D[i][j] = W[i][j];
 	//print W
-	int setDist = 7;
+	int setDist = 10;
 	int setDist2 = 5;
 	char fill = ' ';
 	cout << "Graph: " << endl;
-	cout << "     1         2         3         4         5";
+	cout << "     1";
+	for (int i = 2; i < N + 1; i++) {
+		cout << setw(setDist) << setfill(fill) << i;
+
+	}
 
 	cout << endl << "   ---------------------------------------" << endl;
 	for (int i = 0; i < size(W); i++) {
@@ -27,15 +32,15 @@ int main() {
 		cout << i + 1 << "| ";
 		for (int j = 0; j < size(W[0]); j++) {
 			if (W[i][j] == inf) {
-				cout <<left << setw(setDist2) << setfill(fill) << "inf " << "     ";
+				cout << left << setw(setDist2) << setfill(fill) << "inf " << "     ";
 			}
 			else {
-				cout << left <<setw(setDist2) << setfill(fill) << W[i][j] << "     ";
+				cout << left << setw(setDist2) << setfill(fill) << W[i][j] << "     ";
 			}
 		}
 		cout << endl;
 	}
-	floyd(5, W, D, P);
+	floyd(N, W, D, P);
 	cout << "\n";
 	printSolution(D, P);
 }
